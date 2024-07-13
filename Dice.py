0cc6a9b2638dbb0fda0d162e9dfc487bd5d99ac1
Gashbeen <gashbeen.hassan@gmail.com>
2024-07-13 13:51:59 +0300

    adding images

--- Dice.py
+++ Dice.py
@@ -2,40 +2,39 @@ from tkinter import *
 import random
 
 window = Tk()
-window.title("DICE")
-window.geometry("780x450")
+window.title("ROLL THE DICE")
+window.geometry("380x350")
 window.configure(bg='pale Turquoise2')
-window.resizable(False,False)
+window.resizable(False, False)
+
+# Load dice images
+dice_images = [PhotoImage(file=f'dice{i}.png') for i in range(1, 7)]
 
 def picknumber():
-    lable["text"]= str(random.randint(1,6))
-    lable1["text"]= str(random.randint(1,6))
+    dice1 = random.randint(1, 6)
+    dice2 = random.randint(1, 6)
+    lable.config(image=dice_images[dice1 - 1])
+    lable1.config(image=dice_images[dice2 - 1])
 
-
-button=Button(window, width=30 , height= 8, bg='green', fg='blue', command= picknumber, bd=3 ,
-              relief=SUNKEN, activebackground='white', text='ROLL', font=("bold", 16))
+button = Button(window, width=20, height=2, bg='brown', fg='white', command=picknumber, bd=3,
+                relief=SUNKEN, text='ROLL', font=("bold", 14))
 button.grid(row=0, column=0, padx=10, pady=10)
-button.place(relx=0.5, rely=0.26, anchor=CENTER)
+button.place(relx=0.5, rely=0.12, anchor=CENTER)
 
-lable=Label(window, width=30 , height=8,bg='orange', fg='purple',font=("bold",16) )
+lable = Label(window, width=200, height=150, bg='pale Turquoise2', fg='purple', font=("bold", 16))
 lable.grid(row=1, column=0)
-lable.place(relx=0.5, rely=0.5)
+lable.place(relx=0.01, rely=0.3)
 
-lable1=Label(window, width=30 , height=8,bg='yellow', fg='purple',font=("bold",16) )
+lable1 = Label(window, width=200, height=150, bg='pale Turquoise2', fg='purple', font=("bold", 16))
 lable1.grid(row=1, column=1)
-lable1.place(relx=0.03, rely=0.5)
+lable1.place(relx=0.45, rely=0.3)
 
-lable_dice1=Label(window, text="Dice one" ,width=8 , height=2,bg='yellow', fg='black',font=("bold",16) )
+lable_dice1 = Label(window, text="Dice one", width=8, height=1, bg='pale Turquoise2', fg='black', font=("bold", 16))
 lable_dice1.grid(row=1, column=1)
-lable_dice1.place(relx=0.19, rely=0.5)
+lable_dice1.place(relx=0.14, rely=0.71)
 
-lable_dice2=Label(window, text="Dice two" ,width=8 , height=2,bg='orange', fg='black',font=("bold",16) )
+lable_dice2 = Label(window, text="Dice two", width=8, height=1, bg='pale Turquoise2', fg='black', font=("bold", 16))
 lable_dice2.grid(row=1, column=0)
-lable_dice2.place(relx=0.67, rely=0.5)
-
-
+lable_dice2.place(relx=0.58, rely=0.71)
 
 window.mainloop()
-
-
-
